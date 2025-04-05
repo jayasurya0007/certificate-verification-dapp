312cb6e41f34b6eb1461b7f2327bd5675f451487
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-04-06 00:03:37 +0530

    update package.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,14 +28,14 @@ function LUKSOproject({ Component, pageProps }: AppProps) {
   const { account } = useEthereum();
   return (
     <EthereumProvider>
-      <NetworkProvider>
+      {/* <NetworkProvider>
         <ProfileProvider>
           <RootLayout>
             <NavBar />
             <Component {...pageProps} />
           </RootLayout>
         </ProfileProvider>
-      </NetworkProvider> */}
+      </NetworkProvider>  */}
       <NavBar />
       <RegisterPage />
     </EthereumProvider>
