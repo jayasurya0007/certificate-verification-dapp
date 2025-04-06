5d3b86807525f3edf44c91a750adb36cfdf88f5a
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-04-06 14:16:14 +0530

    Updated the Certificate.nft and studentdashboard.tsx

--- contracts/CertificateNFT.sol
+++ contracts/CertificateNFT.sol
@@ -8,8 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract CertificateNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    
-    // Mapping from token ID to certificate data
+
     struct Certificate {
         string name;
         string institute;
@@ -17,52 +16,68 @@ contract CertificateNFT is ERC721URIStorage, Ownable {
         string certificateType;
         address student;
     }
-    
+
     mapping(uint256 => Certificate) public certificates;
-    
-    // Mapping to track certificates owned by each student
     mapping(address => uint256[]) public studentCertificates;
-    
-    // Only authorized institutes can issue certificates
     mapping(address => bool) public authorizedInstitutes;
-    
+
+    struct CertificateRequest {
+        address student;
+        string name;
+        string message;
+        bool approved;
+    }
+
+    mapping(uint256 => CertificateRequest) public certificateRequests;
+    uint256 public requestCounter;
+
     event CertificateIssued(
-        uint256 indexed tokenId, 
-        address indexed student, 
-        string name, 
+        uint256 indexed tokenId,
+        address indexed student,
+        string name,
         string institute,
         uint256 issueDate,
         string certificateType
     );
-    
+
+    event CertificateRequested(
+        address indexed student,
+        address indexed institute,
+        string name,
+        string message
+    );
+
+    event CertificateRequestApproved(
+        uint256 indexed requestId,
+        address indexed student,
+        address indexed institute
+    );
+
     constructor() ERC721("Academic Certificate", "CERT") Ownable() {}
-    
-    // Owner can authorize institutes
+
     function authorizeInstitute(address institute) external onlyOwner {
         authorizedInstitutes[institute] = true;
     }
-    
-    // Owner can revoke institute authorization
+
     function revokeInstitute(address institute) external onlyOwner {
         authorizedInstitutes[institute] = false;
     }
-    
-    // Issue a new certificate
+
     function issueCertificate(
         address student,
         string memory name,
         string memory institute,
         string memory certificateType,
         string memory tokenURI
-    ) external returns (uint256) {
+    ) public returns (uint256) {
         require(authorizedInstitutes[msg.sender], "Not authorized to issue certificates");
-        
+
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
-        
+
         _mint(student, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
-        
+
         certificates[newTokenId] = Certificate({
             name: name,
             institute: institute,
@@ -70,58 +85,78 @@ contract CertificateNFT is ERC721URIStorage, Ownable {
             certificateType: certificateType,
             student: student
         });
-        
+
         studentCertificates[student].push(newTokenId);
-        
+
         emit CertificateIssued(
-            newTokenId, 
-            student, 
-            name, 
-            institute, 
-            block.timestamp, 
+            newTokenId,
+            student,
+            name,
+            institute,
+            block.timestamp,
             certificateType
         );
-        
+
         return newTokenId;
     }
-    
-    // Get all certificates for a student
+
     function getStudentCertificates(address student) external view returns (uint256[] memory) {
         return studentCertificates[student];
     }
-    
-    // Get certificate details by ID
-    function getCertificateDetails(uint256 tokenId) external view returns (
-        string memory name,
-        string memory institute,
-        uint256 issueDate,
-        string memory certificateType,
-        address student
-    ) {
+
+    function getCertificateDetails(uint256 tokenId)
+        external
+        view
+        returns (
+            string memory name,
+            string memory institute,
+            uint256 issueDate,
+            string memory certificateType,
+            address student
+        )
+    {
         Certificate memory cert = certificates[tokenId];
-        return (
-            cert.name,
-            cert.institute,
-            cert.issueDate,
-            cert.certificateType,
-            cert.student
-        );
+        return (cert.name, cert.institute, cert.issueDate, cert.certificateType, cert.student);
     }
-    
-    // Request certificate (can be implemented for institute approval workflow)
+
     function requestCertificate(
         address institute,
         string memory name,
         string memory message
     ) external {
-        // This is just an event emission, to be caught off-chain by the institute
+        requestCounter++;
+        certificateRequests[requestCounter] = CertificateRequest({
+            student: msg.sender,
+            name: name,
+            message: message,
+            approved: false
+        });
+
         emit CertificateRequested(msg.sender, institute, name, message);
     }
-    
-    event CertificateRequested(
-        address indexed student,
-        address indexed institute,
-        string name,
-        string message
-    );
+
+    function approveCertificateRequest(
+        uint256 requestId,
+        string memory certificateType,
+        string memory tokenURI
+    ) external {
+        CertificateRequest storage request = certificateRequests[requestId];
+
+        require(request.student != address(0), "Invalid request");
+        require(authorizedInstitutes[msg.sender], "Not authorized to approve");
+        require(!request.approved, "Already approved");
+
+        // Mint certificate
+        issueCertificate(
+            request.student,
+            request.name,
+            certificates[_tokenIds.current()].institute,
+            certificateType,
+            tokenURI
+        );
+
+        request.approved = true;
+
+        emit CertificateRequestApproved(requestId, request.student, msg.sender);
+    }
 }
--- contracts/UserRegistry.sol
+++ contracts/UserRegistry.sol
@@ -8,38 +8,42 @@ contract UserRegistry {
     }
 
     mapping(address => User) public users;
-    
+    address[] private registeredAddresses;
+
     event Registered(
         address indexed user,
         string role,
         string metadataHash
     );
 
-    address[] private registeredAddresses;
-
     function registerUser(
         string memory role,
         string memory metadataHash
     ) external {
         require(bytes(users[msg.sender].role).length == 0, "Already registered");
-        registeredAddresses.push(msg.sender); // Add this line
+        registeredAddresses.push(msg.sender);
         users[msg.sender] = User(role, metadataHash);
         emit Registered(msg.sender, role, metadataHash);
     }
 
-    // Add this new function
     function getAllUsers() external view returns (address[] memory) {
         return registeredAddresses;
     }
 
-    function getUser(
-    address userAddress
-        ) external view returns (string memory role, string memory metadataHash) {
-            User memory user = users[userAddress];
-            return (user.role, user.metadataHash);
-        }
-    
+    function getUser(address userAddress)
+        external
+        view
+        returns (string memory role, string memory metadataHash)
+    {
+        User memory user = users[userAddress];
+        return (user.role, user.metadataHash);
+    }
+
     function isUserRegistered(address userAddress) external view returns (bool) {
-    return bytes(users[userAddress].role).length > 0;
-}
+        return bytes(users[userAddress].role).length > 0;
+    }
+
+    function getRole(address userAddress) external view returns (string memory) {
+        return users[userAddress].role;
+    }
 }
