e004f6d71c90b9d3482d8263e87bb398d628d4ec
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-04-06 11:35:57 +0530

    Updated the title

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -17,7 +17,7 @@ const NavBar: React.FC = () => {
                 href="/"
                 className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
               >
-                <span className="font-bold ml-2">LUKSO dApp Boilerplate</span>
+                <span className="font-bold ml-2">LUKSO dApp</span>
               </Link>
             </div>
           </div>
