2db96af63c78901cb9df5d45843c955d53b8677a
jayasurya0007 <115530104+jayasurya0007@users.noreply.github.com>
2025-04-07 19:34:29 +0530

    updated hero

--- src/components/Opening/Hero.tsx
+++ src/components/Opening/Hero.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
 import Image from "next/image"; 
 import ConnectButton from '../ConnectButton2';// Correct import for Next.js Image component
 
